99f0f25e53e797198322b1bad626aebb98ff9860
ashok <ashok03012000@gmail.com>
2023-04-17 15:38:21 +0530

    1st comit

---
+++ src/main/java/spring/controller/HomeController.java
@@ -0,0 +1,19 @@
+package spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	@RequestMapping(value="/home")
+	public String home() {
+		System.out.println("this is home url");
+		return "index";
+	}
+	@RequestMapping(value="/about")
+	public String about() {
+		System.out.println("this is about url");
+		return "about";
+	}
+
+}
